72dee215fa57f1ccdf59fd556adced76bc83b719
eliaberr <eliabe859@gmail.com>
2024-08-01 04:26:12 -0300

    iniciado o nossa historia

--- sitelaiseliabe/src/componentes/pages/home/PageHome.jsx
+++ sitelaiseliabe/src/componentes/pages/home/PageHome.jsx
@@ -1,5 +1,7 @@
 import Nav from '../../layout/nav/nav'
 import style from '../home/PageHome.module.css'
+import NossaHistoria from '../nossa Historia/NossaHistoria'
+
 
 
 
@@ -8,8 +10,9 @@ function PageHome(){
         <div>
            <footer><Nav/></footer>
            <main>
-            <section className={style.imgInicio} >
-                
+            <section className={style.imgInicio}></section>
+            <section className='text-center'>
+                <NossaHistoria/>
             </section>
            </main>
         </div>
