49e469e7c4e021bb627d99737f4c7ed387c35046
Eliabr Rodrigues <eliabe859@gmail.com>
2024-08-01 18:48:17 -0300

    Finalizando o nossa Hitoria

--- sitelaiseliabe/src/componentes/pages/home/PageHome.jsx
+++ sitelaiseliabe/src/componentes/pages/home/PageHome.jsx
@@ -1,6 +1,10 @@
 import Nav from '../../layout/nav/nav'
 import style from '../home/PageHome.module.css'
 import NossaHistoria from '../nossa Historia/NossaHistoria'
+import imgHistoria1 from '../../../img/imgHistoria1.jpg'
+import imgHistoria2 from '../../../img/imgHistoria2.jpg'
+import imgHistoria3 from '../../../img/imgHistoria3.jpg'
+
 
 
 
@@ -11,8 +15,27 @@ function PageHome(){
            <footer><Nav/></footer>
            <main>
             <section className={style.imgInicio}></section>
-            <section className='text-center'>
-                <NossaHistoria/>
+            <section className={`${style.sectionNossaHistoria} text-center `}>
+                <NossaHistoria
+                    txtNossaHistoria="Nossa Hitória"
+                    imgDireita={imgHistoria1}
+                    mensagem="  Tudo começou através dos amigos Ketura e Ederson, que quiseram
+                                apresentar Laís e Eliabe. Após tomar coragem, Eliabe enviou a
+                                primeira mensagem para Laís no dia 20 de outubro de 2020, logo
+                                após ela postar uma foto. Embora tenha ficado 14 dias sem resposta,
+                                no dia 7 de novembro aconteceu o primeiro beijo entre os dois.
+                                A partir daí, começaram a se conhecer melhor e oraram pedindo
+                                a direção de Deus para o namoro."
+                />
+                <NossaHistoria
+                    imgEsquerda={imgHistoria2}
+                    mensagem="No dia 4 de julho de 2021,
+                              Eliabe conversou com o pai de Laís, pedindo a mão dela em namoro,
+                              o que foi aprovado pelas famílias, iniciando uma grande história
+                              de amor entre eles. Depois de um ano e seis meses de namoro,
+                              Eliabe e Laís decidiram dar o próximo passo. Assim, começaram
+                              a orar a Deus, pedindo direção para esse novo momento."
+                />
             </section>
            </main>
         </div>
--- sitelaiseliabe/src/componentes/pages/nossa Historia/NossaHistoria.jsx
+++ sitelaiseliabe/src/componentes/pages/nossa Historia/NossaHistoria.jsx
@@ -3,34 +3,24 @@ import imgHistoria2 from '../../../img/imgHistoria2.jpg'
 import imgHistoria3 from '../../../img/imgHistoria3.jpg'
 import style from '../nossa Historia/NossaHistoria.module.css'
 
-function NossaHistoria() {
+function NossaHistoria({txtNossaHistoria, mensagem, imgDireita, imgEsquerda}) {
     return(
         <div className={` ${style.nossaHistoria}`}>
-            <h1 className={style.txtNossaHistoria}>Nossa História</h1>
-            <div className="d-flex justify-content-center shadow mt-3">
-                <div>
-                    <img src={imgHistoria1} alt="imagem 1" className='shadow rounded-2'/>
+            {txtNossaHistoria ? <h1 className={`pt-5 ${style.txtNossaHistoria}`}>{txtNossaHistoria}</h1>
+            :<></> }
+            
+                <div className={` mt-5 d-flex justify-content-between p-5 container gap-5 shadow mt-3 rounded-5`}>
+                    {imgEsquerda ? <div>
+                        <img src={imgEsquerda} alt="imagem1" className='rounded-3 shadow'/>
+                    </div> : <></> }
+                    
+                    <p className={`${style.boxNossaHistoria} align-content-center`}>
+                        {mensagem}
+                    </p>
+                    {imgDireita ? <div>
+                        <img src={imgDireita} alt="imagem1" className='rounded-3 shadow'/>
+                    </div> : <></> }
                 </div>
-                <div>
-                <p className={style.boxNossaHistoria}>
-                    Tudo começou através dos nossos amigos Ketura e Ederson, que,
-                    após se conhecerem, quiseram nos apresentar. Logo depois de o
-                    noivo Eliabe tomar coragem, ele mandou a primeira mensagem para
-                    Laís, logo após ela postar uma foto, pois tinha acabado de tirar
-                    o siso no dia 20 de outubro de 2020. Eliabe decidiu mandar a mensagem
-                    para Laís, iniciando uma conversa. No entanto, segundo relatos dos
-                    noivos, ele ficou 14 dias no vácuo por ela. Porém, depois disso,
-                    no dia 7 de novembro, aconteceu o primeiro beijo entre os dois. Assim,
-                    começou uma linda história de amor. Logo após o primeiro beijo, começaram
-                    a se conhecer melhor e, após algum tempo, eles começaram a orar, pedindo
-                    a direção de Deus para o seu namoro. No dia 4 de julho de 2021, Eliabe
-                    tomou coragem e foi conversar com o pai de Laís. Chegando lá, ele pediu
-                    a mão dela em namoro para o pai. Sendo assim, o namoro foi aprovado pelos
-                    pais da noiva e pelos pais de Eliabe.
-                    Ali, começou uma grande história de amor entre Laís e Eliabe.
-                </p>
-                </div>
-            </div>
         </div>
     )
 }
