266373960615835447bd026ea005f491f0486c05
eliaberr <eliabe859@gmail.com>
2024-08-08 04:16:59 -0300

    finalizado quase tudo

--- src/componentes/pages/Confirmar Presença/ConfirmarPresenca.jsx
+++ src/componentes/pages/Confirmar Presença/ConfirmarPresenca.jsx
@@ -5,28 +5,26 @@ function ConfirmarPresenca() {
     const [name , setName] = useState('')
     const [convidado, setConvidado] = useState('')
 
-    /*const handleSubmit = (e) => {
+    const handleSubmit = (e) => {
         e.preventDefault();
     
         const phoneNumber = '5511951285687'; // Substitua com seu número
-        const formattedMessage = `  Olá, meu nome é ${name}. Quero Confirmar 
-                                    a Minha Presença no Casamento da Laís e Eliabe
-                                    sou Convidado ${convidado}`;
+        const formattedMessage = `Olá, meu nome é *${name}*. Quero Confirmar a Minha Presença no Casamento da Laís e Eliabe, sou Convidado ${convidado}`;
         const encodedMessage = encodeURIComponent(formattedMessage);
         const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
     
         window.open(whatsappUrl, '_blank');
-      };*/
+      }
 
       console.log(name,convidado)
    return(
     <div className={`col-sm-8 offset-sm-2 rounded-5 shadow text-center p-3 ${style.ConfirmarPresenca}`}>
         <h1>Confirme sua Presença</h1>
-        <div class="col-md-4">
+        <div class="col-md-4 offset-md-4">
             <label for="nomeUser" class="form-label">Digite Seu Nome</label>
             <input type="text" class="form-control" id="nomeUser" placeholder="Digite Seu Nome Aqui" required onChange={(e) => setName(e.target.value)}/>
         </div>
-        <div class="col-md-4">
+        <div class="col-md-4 offset-md-4 mt-3">
             <label htmlFor="">Você é Convidado da:</label>
             <select class="form-select" aria-label="Default select example" onChange={(e) => setConvidado(e.target.value)}>
                 <option selected>Selecione Uma Opção</option>
@@ -34,6 +32,10 @@ function ConfirmarPresenca() {
                 <option value="do Noivo">Eliabe - Noivo</option>
             </select>
         </div>
+
+        <button className={`col-md-3 mt-4 ${style.btnConfirmar}`} onClick={handleSubmit}>
+            Confirmar Presença
+        </button>
 
     </div>
    )
